fbca9c2a0070b06329ca7c0c8017cdce0c70f93a
Jonny <jonnyal96@gmail.com>
2021-05-15 19:33:34 -0500

    Se cambio el cross origin temporalmente de usuarios

--- src/main/java/com/api/Controllers/UsuarioController.java
+++ src/main/java/com/api/Controllers/UsuarioController.java
@@ -21,7 +21,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping ("/api/users")
-@CrossOrigin (origins = "http://localhost:4200")
+@CrossOrigin (origins = "*")
 public class UsuarioController {
 
     @Value ("#{'${path.api.users}'}")
